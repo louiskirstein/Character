d979eb4eba644a60d7732a7aa68599ada8026afa
louiskirstein <louis.kirstein@brentwood.ca>
2015-10-29 18:58:11 -0700

    Eye patch

--- src/character/Cain.java
+++ src/character/Cain.java
@@ -24,6 +24,8 @@ public class Cain {
     }
 
     public void draw(Graphics graphics) {
+        graphics.setColor(Color.WHITE);
+        graphics.drawRect(x, y, width, height);
 
         //arms
         graphics.setColor(Color.GRAY);
@@ -46,14 +48,16 @@ public class Cain {
         graphics.fillOval(x, y, width, height * 70 / 100);
         graphics.setColor(Color.GRAY);
         graphics.drawOval(x, y, width, height * 70 / 100);
+
         //mouth
         graphics.setColor(Color.GRAY);
         graphics.drawOval(x + (width * 40 / 100), y + (height * 45 / 100), width * 20 / 100, height * 20 / 100);
         graphics.setColor(Color.PINK);
         graphics.fillOval(x + (width * 33 / 100), y + (height * 48 / 100), width * 35 / 100, height * 20 / 100);
+
         //tears
-        graphics.setColor(Color.BLUE);
-        graphics.fillRect(x + (width * 17 / 100), y + (height * 40 / 100), width * 22 / 100, height * 30 / 100);
+//        graphics.setColor(Color.BLUE);
+//        graphics.fillRect(x + (width * 17 / 100), y + (height * 40 / 100), width * 22 / 100, height * 30 / 100);
         //eyes
         graphics.setColor(Color.BLACK);
         graphics.fillOval(x + (width * 14 / 100), y + (width * 35 / 100), width * 28 / 100, height * 21 / 100);
@@ -79,12 +83,25 @@ public class Cain {
         graphics.fillOval(x + (width * 18 / 100), y + (width * 38 / 100), width * 13 / 100, height * 9 / 100);
         graphics.fillOval(x + (width * 30 / 100), y + (width * 55 / 100), width * 5 / 100, height * 3 / 100);
 
-        //eye patch
-        //    graphics.setColor(Color.BLACK);
-        //    graphics.fillRect(x + (width * 10 / 100), y + (height * 50 / 100), width * 90 / 100, height * 8 / 100);
         //leg anglet
         graphics.setColor(Color.YELLOW);
         graphics.fillRect(x + (width * 24 / 100), y + (height * 94 / 100), width * 19 / 100, height * 2 / 100);
+
+        //eye patch
+        graphics.setColor(Color.BLACK);
+        int[] xValues = new int[4];
+        xValues[0] = x + (width * 22 / 100);
+        xValues[1] = x + (width * 12 / 100);
+        xValues[2] = x + (width * 100 / 100);
+        xValues[3] = x + (width * 98 / 100);
+
+        int[] yValues = new int[4];
+        yValues[0] = y + (height * 6 / 100);
+        yValues[1] = y + (height * 12 / 100);
+        yValues[2] = y + (height * 33 / 100);
+        yValues[3] = y + (height * 24 / 100);
+
+        graphics.fillPolygon(xValues, yValues, 4);
 
     }
 
--- src/character/CharacterEnvironment.java
+++ src/character/CharacterEnvironment.java
@@ -20,8 +20,8 @@ class CharacterEnvironment extends Environment {
     private Cain bobbyLarge, bobbySmall;
 
     public CharacterEnvironment() {
-        bobbyLarge = new Cain(300, 80, 250, 350);
-        //bobbySmall = new Cain(400, 50, 150, 210 );
+        bobbyLarge = new Cain(100, 80, 250, 350);
+        bobbySmall = new Cain(400, 50, 150, 210 );
         this.setBackground(new Color(200,0,0));
 
     }
@@ -50,10 +50,10 @@ class CharacterEnvironment extends Environment {
     public void paintEnvironment(Graphics graphics) {
         if (bobbyLarge != null) {
             bobbyLarge.draw(graphics);
-
-        //
-        //if (bobbySmall != null) {
-        //    bobbySmall.draw(graphics);
+        }
+        
+        if (bobbySmall != null) {
+            bobbySmall.draw(graphics);
         }
     }
 
